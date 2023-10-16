ffb1de70efccca7a70cf64d9421baf4b4dcb9029
r3verse <sebastian.ohberg@proton.me>
2023-10-16 15:25:47 -0300

    Creating card with javascript

--- website/app/card/page.js
+++ website/app/card/page.js
@@ -1,35 +1,63 @@
-import Image from "next/image";
+import Link from "next/link";
+
+const routes = [
+  {
+    title: "Blog",
+    imgroute: "/metro-video/Blog.gif",
+    route: "/blog",
+  },
+  {
+    title: "Main",
+    imgroute: "/metro-logo/Metrotech1.png",
+    route: "/"
+  },
+  {
+    title: "Test",
+    imgroute: "/metro-logo/Metrotech2.png",
+    route: "quienes-somos"
+  },
+];
 
 export default function Card() {
   return (
-    <div className="flex flex-col max-w-sm rounded overflow-hidden shadow-lg bg-gray-900 hover:scale-105">
-      <div className="flex justify-center">
-        <Image
-          src="/metro-video/Blog.gif"
-          width={250}
-          height={250}
-          alt="Metrotech"
-        />
-      </div>
-
-      <div className="flex flex-col justify-center px-6 py-4">
-        <div className="flex justify-center font-bold text-xl mb-2">Lorem</div>
-        <p className="flex text-gray-400 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-        <div className="flex pt-4">
-          <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300">
-            #photography
-          </span>
-        </div>
+    <div className="pt-5">
+      <div className="flex flex-row justify-center">
+        {routes.map(({ title, imgroute,route }) => (
+          <Link href={route}>
+            <section className="flex flex-col rounded overflow-hidden shadow-lg bg-gray-900 border-2 border-gray-400 max-w-sm">
+              <div className="flex justify-center">
+                <img
+                  className="pt-1 rounded-lg"
+                  src={imgroute}
+                  width={250}
+                  height={250}
+                  alt={title}
+                />
+              </div>
+              <div className="flex flex-col justify-center px-6 py-4">
+                <div className="flex justify-center font-bold text-xl mb-2">
+                  Lorem
+                </div>
+                <p className="flex text-gray-400 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                  exercitationem praesentium nihil.
+                </p>
+                <div className="flex pt-4">
+                  <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300 hover:scale-105">
+                    #photography
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300 hover:scale-105">
+                    #photography
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-sky-300 hover:scale-105">
+                    #photography
+                  </span>
+                </div>
+              </div>
+            </section>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- website/app/page.jsx
+++ website/app/page.jsx
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <>
     <main>
-      <div className="flex flex-row justify-evenly pt-8">
         <Card/>
-        <Card/>
-        <Card/>
-      </div>
     </main>
     <Footer/>
     </>
